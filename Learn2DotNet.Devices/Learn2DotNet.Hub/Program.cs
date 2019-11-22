7842619aed992f9404c3bfee07d700b922b55ebf
Andrei Petrila <petrila.ac@gmail.com>
2019-11-22 11:26:16 +0200

    Create solution

---
+++ Learn2DotNet.Devices/Learn2DotNet.Hub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learn2DotNet.Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
