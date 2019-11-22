25b04cfc7e3766e69a90aae2cada5261dcb1219b
Andrei Petrila <petrila.ac@gmail.com>
2019-11-22 17:52:47 +0200

    Devices UI

---
+++ Learn2DotNet.Devices/Learn2DotNet.Devices/DevicesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Learn2DotNet.Devices.Domain.Model;
+using System.Collections.Generic;
+
+namespace Learn2DotNet.Devices
+{
+    public class DevicesViewModel
+    {
+        public ConnectCommand ConnectCommand { get; } = new ConnectCommand();
+
+        public List<Device> Devices { get; set; } = new List<Device>();
+
+        public DevicesViewModel()
+        {
+            Devices.Add(new Device {Name = "Aspirator 1", Id = new Guid(), DeviceState = DeviceState.Active});
+            Devices.Add(new Device {Name = "Aspirator 2", Id = new Guid(), DeviceState = DeviceState.Inactive});
+        }
+    }
+}
---
+++ Learn2DotNet.Devices/Learn2DotNet.Devices/DevicesWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Learn2DotNet.Devices
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new DevicesViewModel();
+            // GetAutofacContainer();
+        }
+
+        /*private static void GetAutofacContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<Learn2DotNetDevicesRequestBus>().As<IRequestBus>();
+            builder.RegisterType<AutofacRequestHandlerFactory>().As<IRequestHandlerFactory>();
+            builder.RegisterType<DeviceRepository>().As<IDeviceRepository>();
+            builder.Build();
+        }*/
+    }
+}
